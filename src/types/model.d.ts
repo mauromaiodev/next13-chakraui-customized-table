16ae54215beb1f5071ff489be3f4a2804f9ddb8e
Mauro <mauromaio.dev@gmail.com>
2023-09-01 13:38:26 -0300

    feat: model and table types

---
+++ src/types/model.d.ts
@@ -0,0 +1,11 @@
+"use client";
+
+declare module "types/model" {
+  export type User = {
+    id: string;
+    enterprise: string;
+    name: string;
+    position: string;
+    city: string;
+  };
+}
