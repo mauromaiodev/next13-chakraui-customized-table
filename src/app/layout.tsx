272cf6b45bb03f72b17c45809c47a3ae623dca65
Mauro <mauromaio.dev@gmail.com>
2023-09-01 14:01:02 -0300

    removed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import type { Metadata } from "next";
-
 import { ChakraProvider } from "@chakra-ui/react";
-
-export const metadata: Metadata = {
-  title: "Next Template",
-  description: "Template para projetos Next 13"
-};
 
 export default function RootLayout({
   children
