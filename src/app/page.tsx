6537f63815d300b936ba6630339dd2aa32ed49c9
Mauro <mauromaio.dev@gmail.com>
2023-09-01 13:37:17 -0300

    feat: importando data-table e components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,42 @@
+import { Column } from "react-table";
+
+import DataTable from "components/data-table";
+
+import users from "../data/users";
+
+import { Container, Heading } from "@chakra-ui/layout";
+import { User } from "types/model";
+
+const columns: ReadonlyArray<Column<User>> = [
+  {
+    Header: "User ID",
+    accessor: "id"
+  },
+  {
+    Header: "Empresa",
+    accessor: "enterprise"
+  },
+  {
+    Header: "Nome",
+    accessor: "name"
+  },
+  {
+    Header: "Cargo",
+    accessor: "position"
+  },
+  {
+    Header: "Cidade",
+    accessor: "city"
+  }
+];
+
 export default function Home() {
   return (
-    <main>
-      <>Estou na home</>
-    </main>
+    <Container maxW="container.xl" py="12">
+      <Heading as="h1" fontSize="4xl">
+        Clientes
+      </Heading>
+      <DataTable data={users} column={columns} />
+    </Container>
   );
 }
